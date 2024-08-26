8c29188cab2213d9c966916cb752baa195efec1d
sonusatish34 <sonusatish34@gmail.com>
2024-08-26 17:05:22 +0530

    dfg

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <Layout>
        <Head>
+       <link rel="icon" href="/favicon.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Component {...pageProps} />
