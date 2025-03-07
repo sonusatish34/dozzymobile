1858f7446b1aa36e293de203f10b09d7fadc59cd
sonusatish34 <sonusatish34@gmail.com>
2025-03-07 12:51:02 +0530

    ew

--- pages/components/Layout/Layout.js
+++ pages/components/Layout/Layout.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useRouter } from 'next/router';
 import Header from '../Header/Header'; // Adjust path if needed
 import Footer from '../Footer/Footer'; // Adjust path if needed
-
+import Script from 'next/script';
 const Layout = ({ children }) => {
     const router = useRouter();
 
@@ -29,6 +29,29 @@ const Layout = ({ children }) => {
                 <main className="flex-grow bg-white">{children}</main>
                 <Footer />
             </div>
+            <Script
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+            (function(l) {
+              if (!l) { 
+                window.lintrk = function(a, b) { window.lintrk.q.push([a, b]) }; 
+                window.lintrk.q = [];
+              }
+              var s = document.getElementsByTagName("script")[0];
+              var b = document.createElement("script");
+              b.type = "text/javascript";
+              b.async = true;
+              b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+              s.parentNode.insertBefore(b, s);
+            })(window.lintrk);
+          `,
+                }}
+            />
+
+            <noscript>
+                <img height="1" width="1" style={{ display: 'none' }} alt="" src="https://px.ads.linkedin.com/collect/?pid=8006865&fmt=gif" />
+            </noscript>
         </>
     );
 };
